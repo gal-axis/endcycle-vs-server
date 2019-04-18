7334b413d6ef1380374df63b9ad351da17484dc6
Gal Peric <gal.peric@12b3.games>
2019-04-18 17:49:58 +0200

    Updated server.

--- server.bat
+++ server.bat
@@ -1,2 +1,7 @@
 @echo off
-start javaw -server -cp server.jar com.one2b3.endcycle.ui.ServerGUILauncher
+IF EXIST "jre/bin/javaw.exe" (
+	SET javaCommand=jre/bin/javaw.exe
+) ELSE (
+	SET javaCommand=javaw
+)
+start %javaCommand% -server -cp server.jar com.one2b3.endcycle.ui.ServerGUILauncher
